8e4957e4f4718e2f00884c4b8e3910fb57b5d3ce
mehmettekoz <smallcanyon35@gmail.com>
2021-02-05 23:32:59 +0300

    TwiliaERP Group11 project is created

---
+++ src/test/java/com/twiliaerp/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.twiliaerp.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"
+        },
+        features = "src/test/resources/features",
+        glue = "com/twiliaerp/stepdefinitions",
+        dryRun = true,
+        tags = "@login"
+)
+public class CukesRunner {
+}
